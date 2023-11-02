8564a9a193e3765cc8e1a75a0a9eefb4643bf739
Carlos Parra Montoya <79164641+carlokos@users.noreply.github.com>
2023-11-02 13:13:23 +0100

    Delete src/components/FormDialog/ProductoForm directory

--- src/components/FormDialog/ProductoForm/isValid.js
+++
@@ -1,8 +0,0 @@
-const isValid = (formData) => {
-    const { category_id, title, price, weight, stock, color, volume, brand } = formData;
-    return (
-        category_id && title && price && weight && stock && color && volume && brand
-    );
-};
-
-export default isValid;
